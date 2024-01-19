5e1c0418ea5a47d898c016dcc5ad0bdfe0de10e4
eYdr1en <54525514+eYdr1en@users.noreply.github.com>
2024-01-19 21:39:18 +0100

    update multicall

--- balancer-js/src/lib/constants/config.ts
+++ balancer-js/src/lib/constants/config.ts
@@ -496,7 +496,7 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
     addresses: {
       contracts: {
         vault: '0x9Ced3B4E4DC978265484d1F1f569010E13f911c9',
-        multicall: '0x82976578f1841d3055B4460FFd5DFD3a77D22CDC',
+        multicall: '0x69d8D6913Eb2f4d2dCD9E18e7e22a34C15Fb1558',
         balancerHelpers: '0xf0f3b9Eee32b1F490A4b8720cf6F005d4aE9eA86',
         relayerV5: '0x15F707033cA3a44FB3deC7EBC2F3ac5DB3b4e099',
         weightedPoolFactory: '0x981071AA1A430fC1b54C3E171608f88975497cF3',
